92255996a1aa4216f51d829ef2ffec45973192f7
Raghul Prasanth <87120429+Raghul-18@users.noreply.github.com>
2025-07-24 16:38:18 +0530

    Update OpenURLs.java

--- src/com/links/OpenURLs.java
+++ src/com/links/OpenURLs.java
@@ -10,23 +10,18 @@ public class OpenURLs {
         WebDriver driver = new EdgeDriver();
 
         driver.get("https://opensource-demo.orangehrmlive.com/");
-        System.out.println("Page title is: " + driver.getTitle());
 
         driver.switchTo().newWindow(WindowType.TAB);
         driver.get("https://demo.opencart.com/");
-        System.out.println("Page title is: " + driver.getTitle());
 
         driver.switchTo().newWindow(WindowType.TAB);
         driver.get("http://www.bing.com");
-        System.out.println("Page title is: " + driver.getTitle());
 
         driver.switchTo().newWindow(WindowType.TAB);
         driver.get("https://www.freecrm.com/");
-        System.out.println("Page title is: " + driver.getTitle());
 
         driver.switchTo().newWindow(WindowType.TAB);
         driver.get("http://www.half.ebay.com");
-        System.out.println("Page title is: " + driver.getTitle());
 
     }
 }
